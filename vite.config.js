7f0061f31cfd91195d2ace96a5abaa1ed2c9aede
max manackow <max.manackow@gmail.com>
2025-04-20 23:21:45 +0300

    build-test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ChessSite/'
+  // base: '/ChessSite/'
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        settings: resolve(__dirname, 'settings.html'),
+        game: resolve(__dirname, 'game.html'),
+        history: resolve(__dirname, 'history.html'),
+        user: resolve(__dirname, 'user.html'),
+        learn: resolve(__dirname, 'learn.html'),
+      },
+    },
+  },
 })
