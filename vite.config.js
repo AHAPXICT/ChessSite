f18a6ae22d9a34b9cd7f72ed441b56c591d579a7
max manackow <max.manackow@gmail.com>
2025-04-20 23:55:22 +0300

    another...

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import {dirname, resolve} from 'node:path'
+import {fileURLToPath} from 'node:url'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+let __dirname = dirname(fileURLToPath(import.meta.url))
+
+const pages = {
+	main: resolve(__dirname, 'index.html'),
+	settings: 'settings.html',
+	game: resolve(__dirname, 'game.html'),
+	history: resolve(__dirname, 'history.html'),
+	user: resolve(__dirname, 'user.html'),
+	learn: resolve(__dirname, 'learn.html'),
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/ChessSite/',
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        settings: resolve(__dirname, 'settings.html'),
-        game: resolve(__dirname, 'game.html'),
-        history: resolve(__dirname, 'history.html'),
-        user: resolve(__dirname, 'user.html'),
-        learn: resolve(__dirname, 'learn.html'),
-      },
-    },
-    outDir: 'docs',
-  },
+	plugins: [react()],
+	base: '/ChessSite/',
+	build: {
+		rollupOptions: {
+			input: {
+				...pages,
+			},
+		},
+		outDir: 'docs',
+	},
 })
