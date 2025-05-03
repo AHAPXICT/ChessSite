024e9581fc3932e6860bc528af6fc9bcd22685ec
max manackow <max.manackow@gmail.com>
2025-05-03 19:45:58 +0300

    commit

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,10 @@
 import {Link} from "react-router-dom";
 
 export default function NotFound() {
-    <div>
-        <div>Ошибка 404, страница не найдена</div>
-        <Link to="/">Вернуться на главную</Link>
-    </div>
+    return (
+        <div>
+            <div>Ошибка 404, страница не найдена</div>
+            <Link to="/">Вернуться на главную</Link>
+        </div>
+    )
 }
